2794a7c68d8bb33dfd30c175849dcc2248e20d1f
Bartłomiej Krówka <bartek.krowka2@gmail.com>
2025-01-25 15:02:41 +0000

    feat: #dev Mean ready.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+        if len(list) != 9:
+                return ValueError("List must contain nine numbers.")
+        array = np.array(list).reshape(3, 3)
+        mean = [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)]
+        calculations = {}
+        calculations["mean"] = mean
+        return calculations
