fa0722e4854511482b478e21331aa9e01725b31f
Bartłomiej Krówka <bartek.krowka2@gmail.com>
2025-01-25 15:12:25 +0000

    feat: #dev Standard daviation ready.

--- mean_var_std.py
+++ mean_var_std.py
@@ -6,7 +6,9 @@ def calculate(list):
         array = np.array(list).reshape(3, 3)
         mean = [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)]
         variance = [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array)]
+        std_dev = [np.std(array, axis=0).tolist(), np.std(array, axis=1).tolist(), np.std(array)]
         calculations = {}
         calculations["mean"] = mean
         calculations["variance"] = variance
+        calculations["standard deviation"] = std_dev
         return calculations
