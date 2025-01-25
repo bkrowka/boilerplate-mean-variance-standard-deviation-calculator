ce9717075da74b7415411c123726219a2a3a01fe
Bartłomiej Krówka <bartek.krowka2@gmail.com>
2025-01-25 15:17:18 +0000

    feat: #dev Min/max ready.

--- mean_var_std.py
+++ mean_var_std.py
@@ -7,8 +7,12 @@ def calculate(list):
         mean = [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)]
         variance = [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array)]
         std_dev = [np.std(array, axis=0).tolist(), np.std(array, axis=1).tolist(), np.std(array)]
+        max_ = [np.max(array, axis=0).tolist(), np.max(array, axis=1).tolist(), np.max(array)]
+        min_ = [np.min(array, axis=0).tolist(), np.min(array, axis=1).tolist(), np.min(array)]
         calculations = {}
         calculations["mean"] = mean
         calculations["variance"] = variance
         calculations["standard deviation"] = std_dev
+        calculations["max"] = max_
+        calculations["min"] = min_
         return calculations
