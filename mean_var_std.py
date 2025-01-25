be089d2545556501fac3cfa5982a6a50d950f5d2
Bartłomiej Krówka <bartek.krowka2@gmail.com>
2025-01-25 15:28:42 +0000

    refactor: #dev Project ready.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-def calculate(numbers):
-        if len(numbers) != 9:
+def calculate(list):
+        if len(list) != 9:
                 raise ValueError("List must contain nine numbers.")
-        array = np.array(numbers).reshape(3, 3)
+        array = np.array(list).reshape(3, 3)  # [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+        # axis = 0 vertically
+        # axis = 1 horizontally
         calculations = {
                 "mean": [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)],
                 "variance": [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array)],
