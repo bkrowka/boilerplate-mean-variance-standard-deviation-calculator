fa522e94034948e48c2067466618d02b186fba7a
Bartłomiej Krówka <bartek.krowka2@gmail.com>
2025-01-25 15:07:12 +0000

    feat: #dev Variance ready.

--- mean_var_std.py
+++ mean_var_std.py
@@ -5,6 +5,8 @@ def calculate(list):
                 return ValueError("List must contain nine numbers.")
         array = np.array(list).reshape(3, 3)
         mean = [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)]
+        variance = [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array)]
         calculations = {}
         calculations["mean"] = mean
+        calculations["variance"] = variance
         return calculations
