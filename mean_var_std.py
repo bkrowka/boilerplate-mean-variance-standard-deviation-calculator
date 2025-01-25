f246c8c27bb5242225f87fce4250c41f2b1cf7f6
Bartłomiej Krówka <bartek.krowka2@gmail.com>
2025-01-25 15:19:21 +0000

    feat: #dev Sum ready

--- mean_var_std.py
+++ mean_var_std.py
@@ -9,10 +9,12 @@ def calculate(list):
         std_dev = [np.std(array, axis=0).tolist(), np.std(array, axis=1).tolist(), np.std(array)]
         max_ = [np.max(array, axis=0).tolist(), np.max(array, axis=1).tolist(), np.max(array)]
         min_ = [np.min(array, axis=0).tolist(), np.min(array, axis=1).tolist(), np.min(array)]
+        sum_ = [np.sum(array, axis=0).tolist(), np.sum(array, axis=1).tolist(), np.sum(array)]
         calculations = {}
         calculations["mean"] = mean
         calculations["variance"] = variance
         calculations["standard deviation"] = std_dev
         calculations["max"] = max_
         calculations["min"] = min_
+        calculations["sum"] = sum_
         return calculations
