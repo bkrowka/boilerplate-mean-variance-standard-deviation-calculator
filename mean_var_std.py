97bf11a3b71c0def43bf7968eb47f6a4ccb38428
Bartłomiej Krówka <bartek.krowka2@gmail.com>
2025-01-25 15:21:26 +0000

    refactor: #dev Code optimization.

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,20 +1,15 @@
 import numpy as np
 
-def calculate(list):
-        if len(list) != 9:
-                return ValueError("List must contain nine numbers.")
-        array = np.array(list).reshape(3, 3)
-        mean = [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)]
-        variance = [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array)]
-        std_dev = [np.std(array, axis=0).tolist(), np.std(array, axis=1).tolist(), np.std(array)]
-        max_ = [np.max(array, axis=0).tolist(), np.max(array, axis=1).tolist(), np.max(array)]
-        min_ = [np.min(array, axis=0).tolist(), np.min(array, axis=1).tolist(), np.min(array)]
-        sum_ = [np.sum(array, axis=0).tolist(), np.sum(array, axis=1).tolist(), np.sum(array)]
-        calculations = {}
-        calculations["mean"] = mean
-        calculations["variance"] = variance
-        calculations["standard deviation"] = std_dev
-        calculations["max"] = max_
-        calculations["min"] = min_
-        calculations["sum"] = sum_
+def calculate(numbers):
+        if len(numbers) != 9:
+                raise ValueError("List must contain nine numbers.")
+        array = np.array(numbers).reshape(3, 3)
+        calculations = {
+                "mean": [np.mean(array, axis=0).tolist(), np.mean(array, axis=1).tolist(), np.mean(array)],
+                "variance": [np.var(array, axis=0).tolist(), np.var(array, axis=1).tolist(), np.var(array)],
+                "standard deviation": [np.std(array, axis=0).tolist(), np.std(array, axis=1).tolist(), np.std(array)],
+                "max": [np.max(array, axis=0).tolist(), np.max(array, axis=1).tolist(), np.max(array)],
+                "min": [np.min(array, axis=0).tolist(), np.min(array, axis=1).tolist(), np.min(array)],
+                "sum": [np.sum(array, axis=0).tolist(), np.sum(array, axis=1).tolist(), np.sum(array)]
+        }
         return calculations
